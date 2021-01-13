b508524466c79706409b3c8a1767ee63cb062933
William Romero <williamromerovela@gmail.com>
2021-01-13 02:12:58 -0600

    Added some changes

---
+++ 03-first-steps/counter-app/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PrimeraApp from './PrimeraApp';
+
+const divRoot = document.querySelector("#app");
+
+ReactDOM.render(<PrimeraApp />, divRoot);
